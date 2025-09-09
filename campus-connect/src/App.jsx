33fa02798b7743d91b17f8dfe91cf50c9ac4772b
osoba-joshua <joshuaosobaenesi@gmail.com>
2025-09-09 10:29:40 +0100

    fixed all pedning issues made by force push

--- campus-connect/src/App.jsx
+++ campus-connect/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css';
 import Card from './Components/Card';
 import CategoryCards from './Components/CategoryCards';
+import Footer from './Components/Footer';
 import Hero from "./Components/Hero";
 import How from './Components/How';
 
@@ -20,6 +21,7 @@ function App() {
     <TrendingResources/>
     <How/>
     <Notify/>
+    <Footer/>
     </div>
   );
 }
